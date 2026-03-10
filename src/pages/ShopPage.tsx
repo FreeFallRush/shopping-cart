55fe1f7f0486e2cacd6246ce76637b1c573c2a8e
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 18:15:33 +0200

    Imported ProductCard in ShopPage

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,4 +1,5 @@
 import useProducts from "../hooks/useProducts";
+import ProductCard from "../components/ProductCard/ProductCard";
 
 function ShopPage() {
   const { products, loading, error } = useProducts();
@@ -11,7 +12,9 @@ function ShopPage() {
       <div>
         <h1>Shop Page</h1>
         <div className="shop-container">
-          {products.map((product) => product.price)}
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
       </div>
     </>
