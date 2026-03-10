e5ddabceff7be63d1f0139f376015ed7038f3ba7
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 18:31:11 +0200

    Added expandable description in ProductCard component
    Added style to expandable description in ProductCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./ProductCard.module.css";
 import type { Product } from "../../types/types";
 
@@ -6,6 +7,8 @@ interface ProductCardProps {
 }
 
 function ProductCard({ product }: ProductCardProps) {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <>
       <div className={styles.card}>
@@ -16,6 +19,16 @@ function ProductCard({ product }: ProductCardProps) {
         />
         <h3>{product.title}</h3>
         <p className={styles.price}>${product.price}</p>
+
+        <p className={expanded ? styles.fullDescription : styles.description}>
+          {product.description}
+        </p>
+        <button
+          className={styles.readMore}
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ? "Read less" : "Read more"}
+        </button>
         <p className={styles.category}>{product.category}</p>
       </div>
     </>
