034386160c5827971f8e86ce7e01b13cdf604cb5
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 23:28:05 +0200

    Added hero section and featured products section in Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,44 @@
-function Homepage() {
+import type { Product } from "../types/types";
+import styles from "./Pages.module.css";
+import useProducts from "../hooks/useProducts";
+import ProductCard from "../components/ProductCard/ProductCard";
+import { Link, useOutletContext } from "react-router";
+
+interface OutletContextType {
+  addToCart: (product: Product) => void;
+}
+
+const Homepage: React.FC = () => {
+  const { addToCart } = useOutletContext<OutletContextType>();
+  const { products, loading, error } = useProducts();
+
+  if (loading) return <p>Loading products...</p>;
+  if (error) return <p>{error}</p>;
+  const featuredProducts = products.slice(0, 4);
+
   return (
     <>
-      <div>
-        <h1>Homepage</h1>
-      </div>
+      <section className={styles.hero}>
+        <h1>Welcome to EveryShop!</h1>
+        <p>Discover our top-quality products and special deals.</p>
+        <Link to="/shop" className={styles.ctaBtn}>
+          Shop Now
+        </Link>
+      </section>
+
+      <section className={styles.featured}>
+        <h2>Featured Products</h2>
+        <div className={styles.container}>
+          {featuredProducts.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              addToCart={addToCart}
+            />
+          ))}
+        </div>
+      </section>
     </>
   );
-}
+};
 export default Homepage;
