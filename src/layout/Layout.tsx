403899214ed32ec548205cf4b37f164994ae70bf
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 22:47:13 +0200

    Added TypeScript types to Layout and Outlet context

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,32 @@
 import Navbar from "../components/Navbar/Navbar";
 import { Outlet } from "react-router";
+import type { Product } from "../types/types";
 
-function Layout() {
+interface LayoutProps {
+  cart: Product[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (id: number) => void;
+  updateCart: (id: number, quantity: number) => void;
+}
+
+function Layout({
+  cart = [],
+  addToCart,
+  removeFromCart,
+  updateCart,
+}: LayoutProps) {
+  const cartCount = cart.reduce((sum, item) => sum + (item.quantity || 0), 0);
+
   return (
     <div>
-      <Navbar />
+      <Navbar cartCount={cartCount} />
       <main>
-        <Outlet />
+        <Outlet context={{ cart, addToCart, removeFromCart, updateCart }} />
       </main>
+
+      <footer>
+        <p>©EveryShop, 2026 . All rights reserved to the imagination.</p>
+      </footer>
     </div>
   );
 }
