f6059e8232ff00d53efbabb569e2a22b40bda001
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 18:13:27 +0200

    Added ProductCard.module.css
    Imported styles and added types for props in ProductCard component

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,5 +1,18 @@
-function ProductCard() {
-  return <div></div>;
+import styles from "./ProductCard.module.css";
+import type { Product } from "../../types/types";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+function ProductCard({ product }: ProductCardProps) {
+  return (
+    <>
+      <div className={styles.card}>
+        <img src={product.image} alt={product.title} />
+      </div>
+    </>
+  );
 }
 
 export default ProductCard;
