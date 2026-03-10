b1ef780722750b99b4bdebf5140a5eaaa3bf3dce
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 22:11:47 +0200

    Added QuantitySelector folder with QuantitySelector.tsx
    Added QuantitySelector component in QuantitySelector.tsx

---
+++ src/components/QuantitySelector/QuantitySelector.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+interface QuantitySelectorProps {
+  initial?: number;
+  onChange: (quantity: number) => void;
+}
+
+function QuantitySelector({ initial = 1, onChange }: QuantitySelectorProps) {
+  const [quantity, setQuantity] = useState<number>(initial);
+
+  const increment = () => {
+    setQuantity((prev) => {
+      const newQuantity = prev + 1;
+      onChange(newQuantity);
+      return newQuantity;
+    });
+  };
+
+  const decrement = () => {
+    setQuantity((prev) => {
+      const newQuantity = prev > 1 ? prev - 1 : 1;
+      onChange(newQuantity);
+      return newQuantity;
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={increment}>-</button>
+      <span>{quantity}</span>
+      <button onClick={decrement}>+</button>
+    </div>
+  );
+}
+
+export default QuantitySelector;
