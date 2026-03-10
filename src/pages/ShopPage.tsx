3b1d0896ff03a650417846661f481be32923f399
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 23:13:27 +0200

    Added Typescrtipt types and addToCart functionality in ShopPage component

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,8 +1,15 @@
+import { useOutletContext } from "react-router";
 import useProducts from "../hooks/useProducts";
 import ProductCard from "../components/ProductCard/ProductCard";
 import styles from "./Pages.module.css";
+import type { Product } from "../types/types";
+
+interface OutletContextType {
+  addToCart: (product: Product) => void;
+}
 
 function ShopPage() {
+  const { addToCart } = useOutletContext<OutletContextType>();
   const { products, loading, error } = useProducts();
 
   if (loading) return <p>Loading...</p>;
@@ -14,7 +21,11 @@ function ShopPage() {
         <h1>Shop Page</h1>
         <div className={styles.shopContainer}>
           {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard
+              key={product.id}
+              product={product}
+              addToCart={addToCart}
+            />
           ))}
         </div>
       </div>
