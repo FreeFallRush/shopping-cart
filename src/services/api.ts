21f019d5455ca82d2c26d27252bf9b96dbb358bc
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 17:35:09 +0200

    Added return type for getProducts in api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,11 @@
-export async function getProducts() {
+import type { Product } from "../types/types";
+
+export async function getProducts(): Promise<Product[]> {
   const res = await fetch("https://fakestoreapi.com/products");
 
   if (!res.ok) {
     throw new Error("Failed to fetch products");
   }
-  const data = await res.json();
+  const data: Product[] = await res.json();
   return data;
 }
