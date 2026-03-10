ee3e77099ff98653998863751ffeec2181184a15
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 17:53:57 +0200

    Imported useProducts in ShopPage.tsx

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,8 +1,18 @@
+import useProducts from "../hooks/useProducts";
+
 function ShopPage() {
+  const { products, loading, error } = useProducts();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <>
       <div>
         <h1>Shop Page</h1>
+        <div className="shop-container">
+          {products.map((product) => product.price)}
+        </div>
       </div>
     </>
   );
