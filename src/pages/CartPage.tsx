4b43ccc05543a7d35fc9c50b01ed341a6ab0be52
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-11 00:02:46 +0200

    Imported CartItem and used Typescript types in CartPage component

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,8 +1,37 @@
+import { useOutletContext } from "react-router";
+import CartItem from "../components/CartItem/CartItem";
+import type { Product } from "../types/types";
+
+interface OutletContextType {
+  cart: Product[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (id: number) => void;
+  updateCart: (id: number, quantity: number) => void;
+}
+
 function CartPage() {
+  const { cart, removeFromCart, updateCart } =
+    useOutletContext<OutletContextType>();
   return (
     <>
       <div>
         <h1>Cart Page</h1>
+        <div>
+          {cart.length === 0 && (
+            <p> Your cart is empty. Add something from Shop.</p>
+          )}
+
+          <div>
+            {cart.map((item) => (
+              <CartItem
+                key={item.id}
+                product={item}
+                removeFromCart={removeFromCart}
+                updateCart={updateCart}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
