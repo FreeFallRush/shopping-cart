f508ddcc90319da832a7ce28b76aa21020e7bd5d
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 17:05:32 +0200

    Added paths for pages in router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Homepage from "./pages/Homepage.tsx";
+import ShopPage from "./pages/ShopPage.tsx";
+import CartPage from "./pages/CartPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      { index: true, element: <Homepage /> },
+      { path: "shop", element: <ShopPage /> },
+      { path: "cart", element: <CartPage /> },
+    ],
   },
 ]);
 
