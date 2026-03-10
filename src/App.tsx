7c6e70b1f1196c826496051374767b76a632e1c8
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2026-03-10 16:57:20 +0200

    Added links to pages in Navbar.tsx
    Imported Navbar component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <div>
+        <Navbar />
         <Homepage />
       </div>
     </>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router";
+
 function Navbar() {
   return (
     <>
       <nav>
         <div>EveryShop</div>
+        <div>
+          <ul>
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+
+            <Link to="/cart">Cart</Link>
+          </ul>
+        </div>
       </nav>
     </>
   );
